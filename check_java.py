cdc4747c1f37dd488b125e1ae7a4d34ed863f30c
biosefs <jaysonjaramillo010@gmail.com>
2025-05-25 17:07:54 +0800

    Delete check_java.py

--- check_java.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-
-try:
-    result = subprocess.run(['java', '-version'], capture_output=True, text=True)
-    print("Java is installed and in PATH:")
-    print(result.stderr.splitlines()[0])
-except FileNotFoundError:
-    print("Java not found in PATH. Please install Java and add it to your PATH.")
